9323126a5fddb7ad72112a8a98ad2b099859c111
Alman8273 <ajmastrofrancesco@yahoo.com>
2022-04-14 12:47:31 -0400

    - final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class sysInfo
+{
+    static void Main(String [] args)
+    {
+        string machInfo = Environment.MachineName;        //machine info
+        string domainInfo = Environment.UserDomainName;       //current domain
+        string userName = Environment.UserName;            //User
+
+        Console.WriteLine("Machine Name {0}", machInfo);
+        Console.WriteLine("Domain Name {0}", domainInfo);
+        Console.WriteLine("User Name {0}", userName);
+
+    }
+}
